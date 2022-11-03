23c8f708949ab94bd8e2193f1e536a16ce08ec77
kelani34 <kelanitaiwo34@gmail.com>
2022-11-03 10:48:06 +0100

    Style outline color

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -84,7 +84,7 @@ function Contact() {
             ease-in-out
             m-0
             resize-none
-            focus:text-gray-700 focus:bg-white focus:ring-blue-200 focus:ring-4 focus:ring-opacity-50 focus:outline-blue-400 message
+            focus:text-gray-700 focus:bg-white focus:ring-blue-200 focus:ring-4 focus:ring-opacity-50 message
           " id="exampleFormControlTextarea13" rows="3" placeholder="Message" required="required"></textarea>
         </div>
         <div className="form-group form-check text-center mb-6">
