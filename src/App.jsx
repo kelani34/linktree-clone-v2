b23f3e618add212db58f225fb49e122c4a21c9b0
kelani34 <kelanitaiwo34@gmail.com>
2022-11-03 10:08:47 +0100

    Style message box

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,17 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import './App.css'
-import profileImg from './assets/images/profile__img.png'
-import slackIcon from './assets/icons/slack-icon.png'
-import githubIcon from './assets/icons/github-icon.png'
-import shareIcon from './assets/icons/share-icon.svg'
-import menuIcon from './assets/icons/menu-icon.svg'
-import Footer from './components/Footer'
-import Links from './components/Links'
-import { FacebookIcon, TwitterIcon, WhatsappIcon,} from "react-share";
-import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from "react-share";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Tree from './page/Tree'
+import Contact from './page/Contact'
 
 function App() {
-  const [link,  setLink] = useState([
-    {
-      id: 'twitter',
-      text: 'Twiter',
-      display: 'block',
-      link: 'https://twitter.com/kelanicodes'
-    },
-    {
-      id: 'slack',
-      text: 'kelanitaiwo34',
-      display: 'hidden',
-      link: 'https://twitter.com/kelanicodes'
-    },
-    {
-      id: 'btn_zuri',
-      text: 'Zuri Team',
-      link: 'https://training.zuri.team'
-    },
-    {
-      id: 'books',
-      text: 'Checkout really cool books you\'ll love😍',
-      display: 'block',
-      link: 'https://books.zuri.team'
-    },
-    {
-      id: 'book__python',
-      text: 'Read Python books for beginners',
-      display: 'block',
-      link: 'https://books.zuri.team/python-for-beginners?ref_id=kehlani'
-    },
-    {
-      id: 'pitch',
-      text: 'Check out really cool tech talents',
-      display: 'block',
-      link: 'https://background.zuri.team'
-    },
-    {
-      id: 'book__design',
-      text: 'Designer? Checkout these design guide book from Zuri Team',
-      display: 'block',
-      link: 'https://books.zuri.team/design-rules'
-    },
-  ])
   return (
       <>
-        <div>
-          
-          <div className='m-10 mt-25 flex flex-col justify-center items-center relative'>
-            
-            <img id='profile__img' src={profileImg} className='profile-img'/>
-            <h2 className='mt-5 font-bold'>Taiwo Shina-Kelani</h2>
-            <div class="tooltip">
-              <img src={shareIcon} className='absolute share-button'/>
-              <TwitterShareButton url={"Checkout kehlani on https://kelani-linktr-ee.netlify.app"} className='absolute share-button opacity-0 twitter'>
-                  <TwitterIcon size={32} round={true} />
-              </TwitterShareButton>
-              <span className="tooltiptext">Share button</span>
-          </div>
-          <img src={menuIcon} className='absolute menu-icon'/>
-          </div>
-          { link.map((link) => {
-            return (
-              <Links 
-              adding the target="_blank"
-              id={link.id}
-              text= {link.text}
-              link={link.link}
-              display={link.display}
-              />
-            )
-          }) 
-        }
-        <div className='flex mt-4 justify-center space-x-6'>
-          <a href='https://github.com/kelani34'><img src={slackIcon} /></a>
-          <a href='https://hng9.slack.com/'><img src={githubIcon} /></a>
-        </div>
-    </div>
-    <Footer />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Tree />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </Router>
       </>
   )
 }
---
+++ src/page/Contact.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <>
+        <div className='w-full h-screen flex'>
+            <div className='w-6/12 m-auto'>
+                <h1 className='text-3xl font-medium'>Contact me</h1>
+                <p className='text-slate-400 mt-6'>Hi there Ask me about anything you have in mind</p>
+<div className="container my-24 px-6 mx-auto">
+
+  <section className="mb-32 text-center text-gray-800">
+    <div className="max-w-[700px] mx-auto px-3 lg:px-6">
+      <form>
+        <div className="form-group mb-6 flex ">
+          <input type="text" className="form-control block
+            w-6/12
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            inline mr-5
+            focus:text-gray-700 focus:bg-white focus:ring-blue-500/50 focus:outline-none" id="exampleInput7"
+            placeholder="First Name"
+            required="required" />
+          <input type="text" className="form-control block
+            w-6/12
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            inline
+            focus:text-gray-700 focus:bg-white focus:ring-blue-500/50 focus:outline-none" id="exampleInput7"
+            placeholder="First Name"
+            required="required" />
+        </div>
+        <div className="form-group mb-6">
+          <input type="email" className="form-control block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:ring-blue-500/50 focus:outline-none" id="exampleInput8"
+            placeholder="Email address"
+            required="required"
+            pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" />
+        </div>
+        <div className="form-group mb-6">
+          <textarea className="
+            form-control
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            resize-none
+            focus:text-gray-700 focus:bg-white focus:ring-blue-500/50 focus:ring-4 focus:ing-opacity-75 focus:outline-none
+          " id="exampleFormControlTextarea13" rows="3" placeholder="Message" required="required"></textarea>
+        </div>
+        <div className="form-group form-check text-center mb-6">
+          <input type="checkbox"
+            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+            id="exampleCheck87" checked />
+          <label className="form-check-label inline-block text-gray-800" for="exampleCheck87">Send me a copy of this
+            message</label>
+        </div>
+        <button type="submit" className="
+          w-full
+          px-6
+          py-2.5
+          bg-blue-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-blue-700 hover:shadow-lg
+          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-blue-800 active:shadow-lg
+          transition
+          duration-150
+          ease-in-out">Send</button>
+      </form>
+    </div>
+  </section>
+</div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Contact
