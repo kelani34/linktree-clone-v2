080f10a29fd78eab526fbe51319f889f67b10100
kelani34 <kelanitaiwo34@gmail.com>
2022-11-03 11:59:18 +0100

    Style: add style to input box

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -30,6 +30,7 @@ function Contact() {
             focus:text-gray-700 focus:bg-white focus:ring-blue-200 focus:ring-4 focus:ring-opacity-50" id="exampleInput7"
             placeholder="First Name"
             required="required" />
+            <p class="text-red-500 text-xs italic mt-2">Please fill out this field.</p>
             </div>
             <div className='w-6/12'>
                 <label htmlFor="" className='block text-left my-2'>First name</label>
@@ -51,6 +52,7 @@ function Contact() {
             focus:text-gray-700 focus:bg-white focus:ring-blue-200 focus:ring-4 focus:ring-opacity-50" id="exampleInput7"
             placeholder="First Name"
             required="required" />
+            <p class="text-red-500 text-xs italic mt-2">Please fill out this field.</p>
             </div>
         </div>
         <div className="form-group mb-6">
@@ -72,6 +74,7 @@ function Contact() {
             placeholder="Email address"
             required="required"
             pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" />
+            <p class="text-red-500 text-xs italic mt-2">Please fill out this field.</p>
         </div>
         <div className="form-group mb-6">
         <label htmlFor="" className='block text-left my-2'>First name</label>
@@ -92,13 +95,14 @@ function Contact() {
             m-0
             resize-none
             focus:text-gray-700 focus:bg-white focus:ring-blue-200 focus:ring-4 focus:ring-opacity-50 message
-          " id="exampleFormControlTextarea13" rows="3" placeholder="Message" required="required"></textarea>
+          " id="exampleFormControlTextarea13" rows="3" placeholder="Message" required></textarea>
+          <p class="text-red-500 text-xs italic mt-2">Please fill out this field.</p>
         </div>
-        <div className="form-group form-check text-center mb-6">
+        <div className="form-group form-check mb-6">
           <input type="checkbox"
-            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
-            id="exampleCheck87" checked />
-          <label className="form-check-label inline-block text-gray-800" for="exampleCheck87">Send me a copy of this
+            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white  checked:border-blue-600 focus:ring-blue-200 focus:ring-4 focus:ring-opacity-50 transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+            id="exampleCheck87"/>
+          <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck87">Send me a copy of this
             message</label>
         </div>
         <button type="submit" className="
