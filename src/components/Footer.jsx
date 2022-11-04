e5add79a413a4c8d5c6ecd795435608cf67570fd
kelani34 <kelanitaiwo34@gmail.com>
2022-11-04 12:24:12 +0100

    refactor: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,6 @@ function Footer() {
     <div className=''>
       <hr/>
         <div className='p-6  flex  footer'>
-          
         <a href='https://zuri.team' target="_blank" rel="noreferrer noopener"><img src={zuri__logo} /></a>
         <span className='text-slate-400 '>HNG Internship 9 Frontend Task</span>
         <a href='https://hngi9.zuriboard.com/' target="_blank" rel="noreferrer noopener"><img src={I4G__logo} className=''/></a>
